9c06569bc75b54a2710ca8d5f8a74a32e13e45f5
muge-ms1 <murugan63833@gmail.com>
2025-01-08 23:09:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import connectDB from './mongodb.js'
+import userRouter from './userRoute.js'
+
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+app.use('/api/user',userRouter)
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=> console.log('Server started on PORT : '+ port))
